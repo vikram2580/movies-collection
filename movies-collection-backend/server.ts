9f2724f0bac9821315f7f75714e22cb756ad9228
vikram2580 <singh.vikramsingh06@gmail.com>
2024-11-18 14:44:47 -0500

    backend server port changed

--- movies-collection-backend/server.ts
+++ movies-collection-backend/server.ts
@@ -15,7 +15,7 @@ import { AddressInfo } from 'net';
  * Get port from environment and store in Express.
  */
 
-let port = normalizePort(process.env.PORT || '3000');
+let port = normalizePort(process.env.PORT || '5000');
 app.set('port', port);
 
 /**
